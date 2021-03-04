8e6d844ca54a7bc17093e86d2f4e756c252a0e63
bold-class-9124 <kirstiewithrow@gmail.com>
2021-03-04 02:04:56 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,5 @@
 # code your #position_taken? method here!
+board = ["",""," "," ","X"," "," "," ",""]
 def position_taken?(board, index)
   taken = nil
    if (board[index] ==  " " || board[index] == "" || board[index] == nil)
@@ -6,3 +7,5 @@ def position_taken?(board, index)
    else
      taken = true
    end
+
+   
