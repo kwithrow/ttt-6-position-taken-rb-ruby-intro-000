668e2c1c108c55c678904d6a95dc1982a99e1a1e
bold-class-9124 <kirstiewithrow@gmail.com>
2021-03-04 02:06:51 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -7,3 +7,8 @@ def position_taken?(board, index)
    else
      taken = true
    end
+   puts position_taken?(board, 0)
+   puts position_taken?(board, 1)
+   puts position_taken?(board, 2)
+   puts position_taken?(board, 3)
+   puts position_taken?(board, 4)
